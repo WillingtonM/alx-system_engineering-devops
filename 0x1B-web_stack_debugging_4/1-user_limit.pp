6bc4aef6756a6661c91808b4b52c680a43d380ce
WillingtonM <willington.mhlanga@gmail.com>
2023-11-16 20:40:33 +0200

    0x1B. Web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Manuscript enables user holberton to login and open files without error
+
+# Hard file limit for user holberton increase
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Soft file limit for user holberton increase
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
